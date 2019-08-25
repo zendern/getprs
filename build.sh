7fceb2b34c7ce46e7d61e25b7212fef108db2cae
Nathan Zender <github@nathanzender.com>
2019-08-24 20:48:24 -0400

    Build script and moving executables

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name='getprs-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi  
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o distributions/$output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution…'
+        exit 1
+    fi
+done
